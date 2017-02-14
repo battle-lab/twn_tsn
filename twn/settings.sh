a97f9c7761fa457731b6a2a3784023eac04df09a
alorchhota <alorchhota@gmail.com>
2017-02-14 04:36:40 -0500

    python is no longer a prerequisite

--- twn/settings.sh
+++ twn/settings.sh
@@ -10,5 +10,4 @@ threshold=1e-4;
 standardize_data=1;     # 1: standardize data, 0: do not standardize data
 
 matlab_path="matlab";   # "matlab" to use default matlab
-python_path="python";   # "python" to use default python
 rscript_path="Rscript"; # "Rscript" to use default R
